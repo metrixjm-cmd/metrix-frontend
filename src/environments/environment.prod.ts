b4f0eebe8875653cdcd38b41c113d704203b70b1
Tlelo <carlos_tlelo@hotmail.com>
2026-03-25 08:25:57 -0600

    force redeploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ export const environment = {
   // URL del backend Cloud Run — se reemplaza automáticamente en deploy.sh
   // Formato: https://metrix-backend-HASH-uc.a.run.app/api/v1
   // ✅ producción
+  // trigger deploy
 apiUrl: 'https://metrix-backend-249758130960.us-central1.run.app/api/v1'
 };
