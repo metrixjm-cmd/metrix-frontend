de1c2529bc33f803e2943e54d0c1454184031ccb
Tlelo <carlos_tlelo@hotmail.com>
2026-03-25 10:59:16 -0600

    update environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,4 @@
 export const environment = {
   production: true,
-  // URL del backend Cloud Run — se reemplaza automáticamente en deploy.sh
-  // Formato: https://metrix-backend-HASH-uc.a.run.app/api/v1
-  // ✅ producción
-  // trigger deploy
-apiUrl: 'https://metrix-backend-249758130960.us-central1.run.app/api/v1'
+  apiUrl: 'https://metrix-backend-249758130960.us-central1.run.app/api/v1',
 };
